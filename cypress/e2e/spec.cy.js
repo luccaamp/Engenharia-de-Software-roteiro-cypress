02879ad989a33c0911f1e0c5e20ceb4bbf52bea1
luccaamp <luccaamp@gmail.com>
2025-06-03 20:25:05 -0300

    Novos Testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -69,3 +69,121 @@ describe('TODOMvc App', () => {
       .should('have.length', 2);
   });
 });
+
+describe('TodoMVC App - Testes Novos', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+
+  it('Edita uma tarefa existente', () => {
+    // Adiciona uma tarefa
+    cy.get('[data-cy=todo-input]').type('Estudar Cypress{enter}');
+    
+    // Dá duplo clique no label da primeira tarefa para entrar em modo de edição
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .find('label')
+      .dblclick();
+
+    // Edita o texto no input de edição (classe .edit)
+    cy.get('.edit')
+      .clear()
+      .type('Estudar Cypress E2E !!!{enter}');
+    
+    // Valida que o texto da tarefa foi alterado
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .find('label')
+      .should('have.text', 'Estudar Cypress E2E !!!');
+  });
+
+  it('Cancela edição com Esc', () => {
+    // Adiciona uma tarefa
+    cy.get('[data-cy=todo-input]').type('Primeira tarefa{enter}');
+
+    // Duplo clique no label para entrar em modo de edição
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .find('label')
+      .dblclick();
+
+    // Edita o texto, mas cancela com ESC
+    cy.get('.edit')
+      .clear()
+      .type('Texto editado{esc}');
+
+    // Verifica que o texto original permanece
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .find('label')
+      .should('have.text', 'Primeira tarefa');
+  });
+
+  it('Limpeza de Tarefas Completas', () => {
+    // Cria 5 novas tarefas
+    const tarefas = [
+      'Engenharia de Software Prova 2',
+      'Linguagens de Programação Prova 2',
+      'Compiladores 1 Prova 3',
+      'Sexta amigos',
+      'Monitoria'
+    ];
+    tarefas.forEach(tarefa => {
+      cy.get('[data-cy=todo-input]').type(`${tarefa}{enter}`);
+    });
+
+    // Confere a contagem de tarefas
+    cy.get('[data-cy=todos-list]').children().should('have.length', 5);
+
+    // Completa as 3 primeiras tarefas
+    cy.get('[data-cy=todos-list] > li').each(($li, idx) => {
+      if (idx < 3) {
+        cy.wrap($li).find('[data-cy=toggle-todo-checkbox]').click();
+      }
+    });
+
+    // Usa o botão de apagar completas
+    cy.get('.clear-completed').should('be.visible').click();
+
+    // Confere o comportamento das restantes
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 2)
+      .first()
+      .should('contain.text', 'Sexta amigos')
+      .next()
+      .should('contain.text', 'Monitoria');
+  });
+
+  it('Botão que completa todas as tarefas', () => {
+    // Cria 5 novas tarefas
+    const tarefas = [
+      'Engenharia de Software Prova 2',
+      'Linguagens de Programação Prova 2',
+      'Compiladores 1 Prova 3',
+      'Sexta amigos',
+      'Monitoria'
+    ];
+    tarefas.forEach(tarefa => {
+      cy.get('[data-cy=todo-input]').type(`${tarefa}{enter}`);
+    });
+
+    // Clica na opção de marcar todos (label aciona o input .toggle-all)
+    cy.get('.toggle-all-label').click();
+
+    // Itera pela lista checando se todos estão completas (classe 'completed')
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.length', 5)
+      .each(($li) => {
+        cy.wrap($li).should('have.class', 'completed');
+      });
+
+    // Clica novamente para desmarcar todos
+    cy.get('.toggle-all-label').click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .each(($li) => {
+        cy.wrap($li).should('not.have.class', 'completed');
+      });
+  });
+});
